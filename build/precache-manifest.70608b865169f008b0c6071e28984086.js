f2eeeb1f1603b52b999f0498cb3777ec1d7a9c89
John Byrne <jbyrne87@gmail.com>
2021-01-22 00:08:05 -0600

    linked vercel

---
+++ build/precache-manifest.70608b865169f008b0c6071e28984086.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7705d4cf008a548086d9728a3249d372",
+    "url": "/index.html"
+  },
+  {
+    "revision": "311e324c143f7631b293",
+    "url": "/static/css/main.9e5d5dd1.chunk.css"
+  },
+  {
+    "revision": "02c089ebc515a2c1ea5d",
+    "url": "/static/js/2.bc61fcc9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.bc61fcc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "311e324c143f7631b293",
+    "url": "/static/js/main.2d4ba746.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  }
+]);
