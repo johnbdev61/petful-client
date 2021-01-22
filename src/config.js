2433a433f22a453a2df62640b1adacaacd706ecf
John Byrne <jbyrne87@gmail.com>
2021-01-22 00:25:45 -0600

    removed build file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // API_ENDPOINT: heroku
-  API_ENDPOINT: 'http://localhost:8000'
+  REACT_APP_API_BASE='https://petful-client-template.herokuapp.com/api',
+  // API_ENDPOINT: 'http://localhost:8000'
 }
 
 export default config
