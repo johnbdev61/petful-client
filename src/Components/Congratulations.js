33db330653bdbbde21637aca80335dd472f25049
John Byrne <jbyrne87@gmail.com>
2021-02-05 11:41:54 -0600

    fixed link to home

--- src/Components/Congratulations.js
+++ src/Components/Congratulations.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import './Congratulations.css'
 
 export default class Congratulations extends Component {
@@ -6,10 +7,12 @@ export default class Congratulations extends Component {
     const { imgSrc, name, owner} = this.props
     return (
       <div>
-        <img src={imgSrc} alt="pet"/>
+        <img src={imgSrc} alt='pet' />
         <p>Name: {name}</p>
         <p>New Bestie: {owner}</p>
-        <button to='/'>Go to Home</button>
+        <Link to='/'>
+          <button>Go to Home</button>
+        </Link>
       </div>
     )  
   }
